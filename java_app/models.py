d5efe977cae5075a059a7b4badca4c22391765fd
whitchakara <whitneywilkerson@MacBook-Pro.local>
2021-11-12 21:28:42 -0500

    started models not finished

--- java_app/models.py
+++ java_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name= models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    user_name=models.CharField(max_length=20)
+    email=models.CharField(max_length=50)
+    zip_code=models.IntegerField()
+    password=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add="True")
+    updated_at=models.DateTimeField(auto_now="True")
+
+class JavaShops(models.Model):
+    name= models.CharField(max_length=50)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    state= models.CharField(max_length=2)
+    zip_code = models.IntegerField()
+    hours_of_operation= models.IntegerField()
+    phone_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add="True")
+    updated_at = models.DateTimeField(auto_now="True")
