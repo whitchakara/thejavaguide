b81f0f237132633ac86506466c20acddc0047673
whitchakara <whitneywilkerson@MacBook-Pro.lan1>
2021-11-13 18:59:23 -0500

    models for reiew, shop and user

--- java_app/models.py
+++ java_app/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    first_name= models.CharField(max_length=50)
-    last_name=models.CharField(max_length=50)
-    user_name=models.CharField(max_length=20)
-    email=models.CharField(max_length=50)
-    zip_code=models.IntegerField()
-    password=models.CharField(max_length=50)
-    created_at=models.DateTimeField(auto_now_add="True")
-    updated_at=models.DateTimeField(auto_now="True")
 
-class JavaShops(models.Model):
+class Review(models.Model):
+    ambience = models.IntegerField()
+    cleanliness = models.IntegerField()
+    coffee = models.IntegerField()
+    music = models.IntegerField()
+    location = models.IntegerField()
+    additonal_comments = models.TextField()
+    created_at = models.DateTimeField(auto_now_add="True")
+    updated_at = models.DateTimeField(auto_now="True")
+
+
+class JavaShop(models.Model):
     name= models.CharField(max_length=50)
     street_address = models.CharField(max_length=100)
     city = models.CharField(max_length=20)
@@ -19,5 +21,22 @@ class JavaShops(models.Model):
     zip_code = models.IntegerField()
     hours_of_operation= models.IntegerField()
     phone_number = models.IntegerField()
+    reviews = models.ManyToManyField(Review, related_name="reviews")
     created_at = models.DateTimeField(auto_now_add="True")
     updated_at = models.DateTimeField(auto_now="True")
+
+class User(models.Model):
+    first_name= models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    user_name=models.CharField(max_length=20)
+    email=models.CharField(max_length=50)
+    zip_code=models.IntegerField()
+    password=models.CharField(max_length=50)
+    java_shops = models.ManyToManyField(JavaShop, related_name="shops")
+    created_at=models.DateTimeField(auto_now_add="True")
+    updated_at=models.DateTimeField(auto_now="True")
+
+
+
+
+
