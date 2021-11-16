7297ae4cd32b2653943172a6031be220c067838b
whitchakara <whitneywilkerson@MacBook-Pro.local>
2021-11-15 19:44:07 -0500

    get one for shops complete, added backend validations with bcrypt/salt, did routing and views for register, addshopetc

--- java_app/models.py
+++ java_app/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+import bcrypt 
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your models here.
+class JavaManager(models.Manager):
+    def shop_validator(self,postData):
+        errors = {}
+        if len(postData["name"]) < 3:
+            errors["name"] = "  name must be at least 3 characters"
+        if len(postData['street_address']) <6:
+            errors['street_address'] = " address must be at least 6 characters"
+        if len(postData['city']) <3:
+            errors['city'] = " city must be at least 3 characters"
+        if len(postData['state']) <2:
+            errors['city'] = " state must be at least 2 characters"
+        if len(postData['zip_code']) <5:
+            errors['city'] = " Zip Code must be at least 5 characters"
+        if len(postData['hours_of_operation']) <0:
+            errors['city'] = " hours of operation must be included"
+        if len(postData['phone_number']) <10:
+            errors['city'] = " phone number must be at least 10 characters"
+        return errors 
 class JavaShop(models.Model):
     name= models.CharField(max_length=50)
     street_address = models.CharField(max_length=100)
@@ -14,6 +35,41 @@ class JavaShop(models.Model):
     # reviews = models.ForeignKey(Review, related_name="reviews", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add="True")
     updated_at = models.DateTimeField(auto_now="True")
+    JavaManager()
+
+class UserManager(models.Manager):
+    def validate(self, form):
+        errors = {}
+        if len(form['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+        if len(form['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 chacters'
+        if not EMAIL_REGEX.match(form['email']):
+            errors['email'] = 'Invalid Email Address'
+        email_check = self.filter(email = form['email'])
+        if email_check:
+            errors['email'] = "Email already in Use"
+        if len(form['password'])< 8:
+            errors['password'] = 'Password must be at least 8 characters'
+        # if form['password'] != form['confirm']:
+        #     errors['password'] = 'Passwords do not match'
+        return errors
+    def authenticate(self, email, password):
+        users = self.filter(email=email)
+        if not users:
+            return False
+
+        user = users[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+    def register(self, form):
+        pw = bcrypt.hashpw(form['password'].encode(), bcrypt.gensalt()).decode()
+        return self.create(
+            first_name = form['first_name'],
+            last_name = form['last_name'],
+            email = form['email'],
+            password = pw,
+        )
 
 class User(models.Model):
     first_name= models.CharField(max_length=50)
@@ -26,6 +82,7 @@ class User(models.Model):
     # review = models.ForeignKey(Review, related_name="reviews", on_delete = models.CASCADE)
     created_at=models.DateTimeField(auto_now_add="True")
     updated_at=models.DateTimeField(auto_now="True")
+    UserManager()
 
 class Review(models.Model):
     ambience = models.IntegerField()
